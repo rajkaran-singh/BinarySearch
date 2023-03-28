45d65b9097d3a49148561b64f142ae719d3d5a9d
vaibhav <vaibhavkrpandey111@gmail.com>
2023-03-28 23:14:24 +0530

    Added 1st Binary search question

---
+++ Search-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+
+            int  mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
